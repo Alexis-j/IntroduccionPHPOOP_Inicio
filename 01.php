0912213bf12c6957de97c8f7128f84dd981be169
alexis jimenez castillo <alexis.jcastillo@outlook.com>
2024-01-03 18:07:17 +0100

    objetos php

--- 01.php
+++ 01.php
@@ -1,4 +1,30 @@
 <?php include 'includes/header.php';
 
+//definir una clase
+
+class  Producto  {
+    public  $nombre;
+    public  $precio;
+    public  $disponible;
+
+}
+
+$producto = new Producto();
+$producto->nombre = 'Tablet';
+$producto->precio = 200;
+$producto->disponible = true;
+
+echo "<pre>";
+var_dump($producto);
+echo "</pre>";
+
+$producto2 = new Producto();
+$producto2->nombre = 'Monitor Curvo 49';
+$producto2->precio = 500;
+$producto2->disponible = true;
+
+echo "<pre>";
+var_dump($producto2);
+echo "</pre>";
 
 include 'includes/footer.php';
