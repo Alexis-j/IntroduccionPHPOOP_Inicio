aa149e0185380deb04d24f207c7b5d7b7fd7d865
alexis jimenez castillo <alexis.jcastillo@outlook.com>
2024-01-22 13:12:47 +0100

    sentencias preparada

--- 09.php
+++ 09.php
@@ -1,4 +1,32 @@
-<?php include 'includes/header.php';
+<?php
+include 'includes/header.php';
 
+// Conectar a la base de datos
+$db = new mysqli('localhost', 'root', 'llaves', 'bienesraices_crud');
+
+// Crear el query
+$query = "SELECT titulo, imagen FROM propiedades";
+
+// Preparar el statement
+$stmt = $db->prepare($query);
+
+// Ejecutar el statement
+$stmt->execute();
+
+// Crear la variable
+$stmt->bind_result($titulo, $imagen);
+
+// Asignar el resultado
+$stmt->fetch();
+
+// Imprimir el resultado
+while($stmt->fetch()) {
+    var_dump($titulo);
+    var_dump($imagen);
+}
+
+// Cerrar el statement
+$stmt->close();
 
 include 'includes/footer.php';
+?>
